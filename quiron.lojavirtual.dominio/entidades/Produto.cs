139d2148cb5740c740f094d614cb3daf8a733c89
Willian Cevassi <willian.cevassi@outlook.com.br>
2016-12-21 16:37:57 -0200

    Criação da Classe Produto

---
+++ quiron.lojavirtual.dominio/entidades/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiron.lojavirtual.dominio.entidades
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+
+        public string Categoria { get; set; }
+
+
+    }
+}
