506c7dc2466eda41417f35740bfb27eb55aa5939
Willian Cevassi <willian.cevassi@outlook.com.br>
2016-12-21 19:58:56 -0200

    criacao do context para o Entity Framework

---
+++ quiron.lojavirtual.dominio/repositorio/EFDBContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using quiron.lojavirtual.dominio.entidades;
+
+namespace quiron.lojavirtual.dominio.repositorio
+{
+    public class EfdbContext:DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
